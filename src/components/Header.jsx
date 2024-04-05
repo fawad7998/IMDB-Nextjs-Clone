8f0cef22f8bdbc37bc00bb4606c7a60e4b6beaa6
Fawad Ahmad <fawad7998@gmail.com>
2024-04-06 01:46:58 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import MenuItem from './MenuItem';
+import { AiFillHome } from 'react-icons/ai';
+import { BsFillInfoCircleFill } from 'react-icons/bs';
+import DarkModeSwitch from './DarkModeSwitch';
+
+export default function Header() {
+  return (
+    <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
+      <div className='flex gap-4'>
+        <MenuItem title='home' address='/' Icon={AiFillHome} />
+        <MenuItem title='about' address='/about' Icon={BsFillInfoCircleFill} />
+      </div>
+      <div className='flex items-center gap-4'>
+        <DarkModeSwitch />
+        <Link href={'/'} className='flex gap-1 items-center'>
+          <span className=' bg-amber-500 py-1 px-2 rounded-lg'>
+            <div className='text-1xl font-bold px-2 '>
+              Fawad
+            </div>
+            <span className='text-2xl font-bold py-1 px-2 rounded-lg'>
+              IMDb
+            </span>
+          </span>
+          <span className='text-xl hidden sm:inline'>Clone</span>
+        </Link>
+      </div>
+    </div >
+  );
+}
